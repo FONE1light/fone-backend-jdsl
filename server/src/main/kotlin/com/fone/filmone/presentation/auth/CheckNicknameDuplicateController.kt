728e021bbfc2bf3d0d21bf08f3003250e5e6c2f4
Bob Sin <tkagmd1@naver.com>
2022-12-28 01:40:14 +0900

    [#25] GetMapping으로 변경 (#28)

--- server/src/main/kotlin/com/fone/filmone/presentation/auth/CheckNicknameDuplicateController.kt
+++ server/src/main/kotlin/com/fone/filmone/presentation/auth/CheckNicknameDuplicateController.kt
@@ -4,6 +4,8 @@ import com.fone.filmone.application.auth.CheckNicknameDuplicationFacade
 import com.fone.filmone.common.response.CommonResponse
 import com.fone.filmone.presentation.auth.CheckNicknameDuplicateDto.CheckNicknameDuplicateRequest
 import com.fone.filmone.presentation.auth.CheckNicknameDuplicateDto.CheckNicknameDuplicateResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,8 +17,8 @@ class CheckNicknameDuplicateController(
     private val checkNicknameDuplicateFacade: CheckNicknameDuplicationFacade
 ) {
 
-    @PostMapping("/check-nickname-duplication")
-    suspend fun checkNickname(@RequestBody request: CheckNicknameDuplicateRequest):
+    @GetMapping("/check-nickname-duplication")
+    suspend fun checkNickname(@ModelAttribute request: CheckNicknameDuplicateRequest):
             CommonResponse<CheckNicknameDuplicateResponse> {
         val response = checkNicknameDuplicateFacade.check(request)
         return CommonResponse.success(response)
