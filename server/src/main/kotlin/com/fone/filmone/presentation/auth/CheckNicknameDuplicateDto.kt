1d3ed0b90cd3934db5171cc380f7c682292ecc91
Bob Sin <tkagmd1@naver.com>
2023-01-07 01:29:18 +0900

    [feat-40] add validation (#43)

--- server/src/main/kotlin/com/fone/filmone/presentation/auth/CheckNicknameDuplicateDto.kt
+++ server/src/main/kotlin/com/fone/filmone/presentation/auth/CheckNicknameDuplicateDto.kt
@@ -1,8 +1,11 @@
 package com.fone.filmone.presentation.auth
 
+import javax.validation.constraints.NotEmpty
+
 class CheckNicknameDuplicateDto {
 
     data class CheckNicknameDuplicateRequest(
+        @field:NotEmpty(message = "닉네임은 필수 값 입니다.")
         val nickname: String,
     )
 
