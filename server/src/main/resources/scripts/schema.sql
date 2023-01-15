f00ad4c03146992b2c271225accd0935997653bd
Bob Sin <tkagmd1@naver.com>
2023-01-15 22:28:44 +0900

    [feat-65] 구인구직 table ddl (#66)

--- server/src/main/resources/scripts/schema.sql
+++ server/src/main/resources/scripts/schema.sql
@@ -30,3 +30,40 @@ CREATE TABLE questions
     agree_to_personal_information tinyint(1),
     primary key (id)
 );
+
+DROP TABLE IF EXISTS job_openings;
+CREATE TABLE job_openings
+(
+    id                 bigint NOT NULL AUTO_INCREMENT,
+    title              varchar(30),
+    interests          varchar(50),
+    deadline           varchar(50),
+    casting            varchar(30),
+    number_of_recruits int(11),
+    gender             varchar(10),
+    age_max            int(11),
+    age_min            int(11),
+    career             varchar(20),
+    type               varchar(10),
+    domains            varchar(200),
+    primary key (id)
+);
+
+DROP TABLE IF EXISTS works;
+CREATE TABLE works
+(
+    id             bigint NOT NULL AUTO_INCREMENT,
+    produce        varchar(50),
+    title          varchar(50),
+    director       varchar(50),
+    genre          varchar(20),
+    logline        varchar(50),
+    location       varchar(200),
+    period varchar (100),
+    pay            varchar(50),
+    details        varchar(500),
+    manager        varchar(50),
+    email          varchar(20),
+    job_opening_id bigint,
+    primary key (id)
+);
