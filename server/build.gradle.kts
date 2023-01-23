9209de0db6173bdf8fc568f60cc7e907b0c1a540
sinkyoungdeok <tkagmd1@naver.com>
2023-01-23 23:39:18 +0900

    [feat-1] r2dbc -> jdsl configuration

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -14,10 +14,15 @@ dependencies {
     val jdslVersion = "2.1.0.RELEASE"
     implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:$jdslVersion")
 
-    // h2 db reactive
+    // mysql db reactive
     implementation("io.agroal:agroal-pool:2.0")
     implementation("mysql:mysql-connector-java")
-    implementation("io.vertx:vertx-jdbc-client:4.3.7")
+    implementation("io.vertx:vertx-mysql-client:4.3.1")
+
+    // reactive
+    implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-hibernate-reactive:$jdslVersion")
+    implementation("org.hibernate.reactive:hibernate-reactive-core:1.1.9.Final")
+    implementation("io.smallrye.reactive:mutiny-kotlin:2.0.0")
 
     // lombok
     implementation("org.projectlombok:lombok")
