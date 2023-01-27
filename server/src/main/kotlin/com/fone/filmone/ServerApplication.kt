9983fcf5cbfa18a06dd9686174c78bf9a084ce6d
Bob Sin <tkagmd1@naver.com>
2023-01-28 02:51:16 +0900

    [feat-42] Home 화면 Aggregator 서버 세팅 (#43)

--- server/src/main/kotlin/com/fone/filmone/ServerApplication.kt
+++ server/src/main/kotlin/com/fone/filmone/ServerApplication.kt
@@ -4,8 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-@ConfigurationPropertiesScan
+@SpringBootApplication(scanBasePackages = ["com.fone"])
 class ServerApplication
 
 fun main(args: Array<String>) {
