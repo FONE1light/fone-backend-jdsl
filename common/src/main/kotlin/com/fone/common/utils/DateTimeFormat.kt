db9f7378d18cb4157dba1e3e24dcaea123a70eb5
Bob Sin <tkagmd1@naver.com>
2023-01-29 23:57:09 +0900

    [feat-52] 프로필 API 만나이 추가 (#53)

--- common/src/main/kotlin/com/fone/common/utils/DateTimeFormat.kt
+++ common/src/main/kotlin/com/fone/common/utils/DateTimeFormat.kt
@@ -1,8 +1,10 @@
 package com.fone.common.utils
 
+import java.sql.Date
 import java.time.LocalDate
 import java.time.Period
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 class DateTimeFormat(
 ) {
@@ -10,14 +12,29 @@ class DateTimeFormat(
     companion object {
         private val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
-        fun calculate(deadline: LocalDate?): String {
+        fun calculateDays(deadline: LocalDate?): String {
             val diffDays = Period.between(
                 LocalDate.parse(LocalDate.now().toString(), dateFormatter),
                 LocalDate.parse(deadline.toString(), dateFormatter)
             ).days
 
 
-            return if(diffDays >= 0) "D-$diffDays" else "마감"
+            return if (diffDays >= 0) "D-$diffDays" else "마감"
+        }
+
+        fun calculateAge(date: LocalDate?): Int {
+            val birthCalendar = Calendar.getInstance()
+            birthCalendar.time = Date.valueOf(date.toString())
+            val current = Calendar.getInstance()
+            val currentYear = current[Calendar.YEAR]
+            val currentMonth = current[Calendar.MONTH]
+            val currentDay = current[Calendar.DAY_OF_MONTH]
+            var age = currentYear - birthCalendar[Calendar.YEAR]
+            if (birthCalendar[Calendar.MONTH] * 100 +
+                birthCalendar[Calendar.DAY_OF_MONTH] > currentMonth * 100 + currentDay
+            ) age--
+
+            return age
         }
     }
 
