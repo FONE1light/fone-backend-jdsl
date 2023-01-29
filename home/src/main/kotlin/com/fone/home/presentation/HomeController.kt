0d6e61dd2a6f0ead542250b53c276d7ad5488bb9
sinkyoungdeok <tkagmd1@naver.com>
2023-01-30 01:22:43 +0900

    [feat-56] 홈화면 API 기본 틀 완성

--- home/src/main/kotlin/com/fone/home/presentation/HomeController.kt
+++ home/src/main/kotlin/com/fone/home/presentation/HomeController.kt
@@ -1,17 +1,68 @@
 package com.fone.home.presentation
 
+import com.fone.common.response.CommonResponse
 import io.swagger.annotations.Api
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
 
 @Api(tags = ["07. Home Info"], description = "홈 서비스")
 @RestController
 @RequestMapping("/home/v1/home")
 class HomeController {
 
+    val client = HttpClient.create()
+        .responseTimeout(Duration.ofSeconds(1))
+
+    val webClient: WebClient = WebClient.builder()
+        .clientConnector(ReactorClientHttpConnector(client))
+        .baseUrl("http://localhost:8080")
+        .build()
+
     @GetMapping
-    suspend fun test(): String {
-        return "test"
+    suspend fun test(
+        @RequestHeader(
+            value = "Authorization",
+            required = false
+        ) token: String,
+    ): CommonResponse<HomeDto> {
+        val userResponse = webClient.get()
+            .uri("/api/v1/user")
+            .header("Authorization", token)
+            .retrieve()
+            .bodyToMono(CommonResponse::class.java)
+
+        val jobOpeningResponse = webClient.get()
+            .uri("/api/v1/job-openings/my-similar")
+            .header("Authorization", token)
+            .retrieve()
+            .bodyToMono(CommonResponse::class.java)
+
+        val competitionResponse = webClient.get()
+            .uri("/api/v1/competitions")
+            .header("Authorization", token)
+            .retrieve()
+            .bodyToMono(CommonResponse::class.java)
+
+        val profileResponse = webClient.get()
+            .uri("/api/v1/profiles?type=ACTOR")
+            .header("Authorization", token)
+            .retrieve()
+            .bodyToMono(CommonResponse::class.java)
+
+        val response = HomeDto(
+            user = userResponse.awaitSingle().data,
+            jobOpenings = jobOpeningResponse.awaitSingle().data,
+            competitions = competitionResponse.awaitSingle().data,
+            profiles = profileResponse.awaitSingle().data,
+        )
+
+        return CommonResponse.success(response)
     }
 }
