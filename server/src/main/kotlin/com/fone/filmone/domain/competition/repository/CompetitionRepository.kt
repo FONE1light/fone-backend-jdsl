dd91d1c2aeaf9301fbb882c1ae9069013ce46a6d
sinkyoungdeok <tkagmd1@naver.com>
2023-02-01 23:48:37 +0900

    [feat-69] 공모전 API에 데이터 전체 갯수 정보 추가

--- server/src/main/kotlin/com/fone/filmone/domain/competition/repository/CompetitionRepository.kt
+++ server/src/main/kotlin/com/fone/filmone/domain/competition/repository/CompetitionRepository.kt
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Slice
 
 interface CompetitionRepository {
     suspend fun findAll(pageable: Pageable): Slice<Competition>
+    suspend fun count(): Long
     suspend fun findById(competitionId: Long): Competition?
     suspend fun findScrapAllById(
         pageable: Pageable,
