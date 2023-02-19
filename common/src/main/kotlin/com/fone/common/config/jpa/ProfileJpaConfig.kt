b4408010a0f4b864c7d0d4bd65a264d9db2dfca7
sinkyoungdeok <tkagmd1@naver.com>
2023-02-19 10:20:32 +0900

    [feat-139] 모듈분리 - 유저 에러 수정

---
+++ common/src/main/kotlin/com/fone/common/config/jpa/ProfileJpaConfig.kt
@@ -0,0 +1,10 @@
+package com.fone.common.config.jpa
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EntityScan(basePackages = ["com.fone.filmone.domain.profile"])
+@EnableJpaRepositories(basePackages = ["com.fone.filmone.infrastructure.profile"])
+class ProfileJpaConfig
