a51d8da9ddeed959bc41ba6b7156a233fb520f89
sinkyoungdeok <tkagmd1@naver.com>
2023-02-19 20:11:05 +0900

    [feat-154] User DTO response 통일

--- user/src/main/kotlin/com/fone/user/presentation/RetrieveMyPageUserDto.kt
+++ user/src/main/kotlin/com/fone/user/presentation/RetrieveMyPageUserDto.kt
@@ -10,35 +10,13 @@ import java.time.LocalDate
 class RetrieveMyPageUserDto {
 
     data class RetrieveMyPageUserResponse(
-        val job: Job,
-        val interests: List<CategoryType>,
-        val nickname: String,
-        val birthday: LocalDate?,
-        val gender: Gender,
-        val profileUrl: String,
-        val phoneNumber: String,
-        val email: String,
-        val socialLoginType: SocialLoginType,
-        val agreeToTermsOfServiceTermsOfUse: Boolean,
-        val agreeToPersonalInformation: Boolean,
-        val isReceiveMarketing: Boolean,
+        val user: UserDto,
     ) {
 
         constructor(
             user: User,
         ) : this(
-            job = user.job,
-            interests = user.interests.map { CategoryType(it) }.toList(),
-            nickname = user.nickname,
-            birthday = user.birthday,
-            gender = user.gender,
-            profileUrl = user.profileUrl,
-            phoneNumber = user.phoneNumber,
-            email = user.email,
-            socialLoginType = user.socialLoginType,
-            agreeToTermsOfServiceTermsOfUse = user.agreeToTermsOfServiceTermsOfUse,
-            agreeToPersonalInformation = user.agreeToPersonalInformation,
-            isReceiveMarketing = user.isReceiveMarketing,
+            user = UserDto(user)
         )
     }
 }
