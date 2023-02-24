66fc5f19e9747a99438e5d46cb75f62004be4dbd
sinkyoungdeok <tkagmd1@naver.com>
2023-02-24 18:24:14 +0900

    [feat-179] 테스트코드 샘플 추가

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -2,8 +2,23 @@ plugins {
     kotlin("plugin.jpa") version "1.7.0"
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
+
 dependencies {
     implementation(project(path = ":common", configuration = "default"))
+    // build.gradle.kts
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
+    testImplementation(kotlin("test"))
+
+    val coroutineVersion = "1.6.3"
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutineVersion")
 }
 
 tasks.bootJar { enabled = false }
---
+++ user/src/test/kotlin/com/fone/user/presentation/controller/SignUpUserControllerTest.kt
@@ -0,0 +1,52 @@
+package com.fone.user.presentation.controller
+
+import com.fone.common.entity.CategoryType
+import com.fone.common.entity.Gender
+import com.fone.user.domain.enum.Job
+import com.fone.user.domain.enum.SocialLoginType
+import com.fone.user.presentation.dto.SignUpUserDto.SignUpUserRequest
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.body
+import reactor.core.publisher.Mono
+import java.time.LocalDate
+
+@WebFluxTest(SignUpUserController::class)
+@ExternalCoroutineApi
+internal class SignUpUserControllerTest {
+
+    @Autowired
+    private lateinit var wtc: WebTestClient
+
+    @Test
+    fun `sign up user`() {
+        val signUpUserRequest = SignUpUserRequest(
+            Job.ACTOR,
+            listOf(CategoryType.ETC),
+            "test5",
+            LocalDate.now(),
+            Gender.IRRELEVANT,
+            null,
+            "010-1234-1234",
+            "test5@test.com",
+            SocialLoginType.APPLE,
+            true,
+            true,
+            true,
+            "test",
+        )
+
+        wtc.post().uri("/api/v1/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .body(Mono.just(signUpUserRequest))
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .consumeWith { println(it) }
+            .jsonPath("$.result").isNotEmpty
+    }
+}
