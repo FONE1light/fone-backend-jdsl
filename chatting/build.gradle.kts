93617e913ad62c74c8173b74d1dfe0c700f22a2c
sinkyoungdeok <tkagmd1@naver.com>
2023-02-24 11:38:46 +0900

    [feat-152] dependency 주석 정리

--- chatting/build.gradle.kts
+++ chatting/build.gradle.kts
@@ -1,6 +1,10 @@
 dependencies {
+
+    // Webflux
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,25 +1,25 @@
 dependencies {
-    // persistence
+    // Persistence
     implementation("javax.persistence:javax.persistence-api")
 
-    // swagger
+    // Swagger
     implementation("io.springfox:springfox-boot-starter:3.0.0")
 
-    // lombok
+    // Lombok
     implementation("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
-    // mysql db reactive
+    // Mysql DB Reactive
     implementation("io.agroal:agroal-pool:2.0")
     implementation("mysql:mysql-connector-java")
     implementation("io.vertx:vertx-mysql-client:4.3.7")
 
-    // kotlin-jdsl
+    // Kotlin-jdsl
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     val jdslVersion = "2.2.0.RELEASE"
     implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:$jdslVersion")
 
-    // reactive
+    // Reactive
     implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-hibernate-reactive:$jdslVersion")
     implementation("org.hibernate.reactive:hibernate-reactive-core:1.1.9.Final")
     implementation("io.smallrye.reactive:mutiny-kotlin:1.6.0")
@@ -27,10 +27,10 @@ dependencies {
     // Kotlin 로깅
     implementation("io.github.microutils:kotlin-logging:2.1.21")
 
-    // security
+    // Security
     implementation("org.springframework.boot:spring-boot-starter-security")
 
-    // redis
+    // Redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     // JWT 인증
@@ -38,11 +38,11 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
-    // validation
+    // Validation
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.hibernate.validator:hibernate-validator:6.1.2.Final")
 
-    // webflux
+    // Webflux
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
