8858642d007b89c0e9855446f6c7617c043d92c0
Bob Sin <tkagmd1@naver.com>
2023-03-01 17:41:27 +0900

    [#189] 문의등록 통합테스트 추가 (#190)
    * [#189] 문의등록 통합테스트 추가
    
    * [#189] spotless

---
+++ server/src/test/kotlin/com/fone/question/presentation/controller/RegisterQuestionControllerTest.kt
@@ -0,0 +1,27 @@
+package com.fone.question.presentation.controller
+
+import com.fone.common.CommonCallApi
+import com.fone.common.CustomDescribeSpec
+import com.fone.common.IntegrationTest
+import com.fone.common.doPost
+import com.fone.question.domain.enum.Type
+import com.fone.question.presentation.dto.RegisterQuestionDto.*
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@IntegrationTest
+class RegisterQuestionControllerTest(client: WebTestClient) : CustomDescribeSpec() {
+
+    private val registerUrl = "/api/v1/questions"
+
+    init {
+        val (accessToken, email) = CommonCallApi.getAccessToken(client)
+        val registerQuestionRequest =
+            RegisterQuestionRequest(email, Type.ALLIANCE, "테스트 제목", "테스트 설명", true)
+
+        describe("#register question") {
+            context("유효한 정보로 문의등록을 하면") {
+                it("성공한다") { client.doPost(registerUrl, registerQuestionRequest, accessToken) }
+            }
+        }
+    }
+}
