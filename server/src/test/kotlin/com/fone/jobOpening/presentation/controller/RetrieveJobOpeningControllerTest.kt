07ecf9ddc0e011fcf144fe9b88c200fc959fd92c
sinkyoungdeok <tkagmd1@naver.com>
2023-03-05 22:29:46 +0900

    [#192] 구인구직 조회 테스트코드 추가

--- server/src/test/kotlin/com/fone/jobOpening/presentation/controller/RetrieveJobOpeningControllerTest.kt
+++ server/src/test/kotlin/com/fone/jobOpening/presentation/controller/RetrieveJobOpeningControllerTest.kt
@@ -1,8 +1,59 @@
 package com.fone.jobOpening.presentation.controller
 
+import com.fone.common.CommonJobOpeningCallApi
+import com.fone.common.CommonUserCallApi
 import com.fone.common.CustomDescribeSpec
 import com.fone.common.IntegrationTest
+import com.fone.common.doGet
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @IntegrationTest
-class RetrieveJobOpeningControllerTest(client: WebTestClient) : CustomDescribeSpec()
+class RetrieveJobOpeningControllerTest(client: WebTestClient) : CustomDescribeSpec() {
+
+    private val retrieveUrl = "/api/v1/job-openings"
+
+    init {
+        val (accessToken, email) = CommonUserCallApi.getAccessToken(client)
+        val jobOpeningId = CommonJobOpeningCallApi.register(client, accessToken)
+
+        describe("#retrieve jobOpenings") {
+            context("구인구직 리스트를 조회하면") {
+                it("성공한다") {
+                    client
+                        .doGet(retrieveUrl, accessToken, mapOf("type" to "ACTOR"))
+                        .expectStatus().isOk
+                        .expectBody()
+                        .consumeWith { println(it) }
+                        .jsonPath("$.result")
+                        .isEqualTo("SUCCESS")
+                }
+            }
+        }
+
+        describe("#retrieve jobOpening") {
+            context("존재하는 구인구직을 상세 조회하면") {
+                it("성공한다") {
+                    client
+                        .doGet("$retrieveUrl/$jobOpeningId", accessToken, mapOf("type" to "ACTOR"))
+                        .expectStatus().isOk
+                        .expectBody()
+                        .consumeWith { println(it) }
+                        .jsonPath("$.result")
+                        .isEqualTo("SUCCESS")
+                }
+            }
+
+            context("존재하지 않는 구인구직을 상세 조회하면") {
+                it("실패한다") {
+                    client
+                        .doGet("$retrieveUrl/1231", accessToken, mapOf("type" to "ACTOR"))
+                        .expectStatus().isOk
+                        .expectBody()
+                        .consumeWith { println(it) }
+                        .jsonPath("$.result")
+                        .isEqualTo("FAIL")
+                }
+            }
+        }
+    }
+}
