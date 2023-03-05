043cbf227f1cc229f2a00d4ab3b08120b912fef8
sinkyoungdeok <tkagmd1@naver.com>
2023-03-05 22:36:00 +0900

    [#192] 내가 등록한 구인구직 조회 테스트코드 추가

--- server/src/test/kotlin/com/fone/jobOpening/presentation/controller/RetrieveJobOpeningMyRegistrationControllerTest.kt
+++ server/src/test/kotlin/com/fone/jobOpening/presentation/controller/RetrieveJobOpeningMyRegistrationControllerTest.kt
@@ -1,9 +1,28 @@
 package com.fone.jobOpening.presentation.controller
 
+import com.fone.common.CommonJobOpeningCallApi
+import com.fone.common.CommonUserCallApi
 import com.fone.common.CustomDescribeSpec
 import com.fone.common.IntegrationTest
+import com.fone.common.doGet
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @IntegrationTest
-class RetrieveJobOpeningMyRegistrationControllerTest(client: WebTestClient) :
-    CustomDescribeSpec()
+class RetrieveJobOpeningMyRegistrationControllerTest(client: WebTestClient) : CustomDescribeSpec() {
+
+    private val retrieveMyRegistrationsUrl = "/api/v1/job-openings/my-registrations"
+
+    init {
+        val (accessToken, email) = CommonUserCallApi.getAccessToken(client)
+        CommonJobOpeningCallApi.register(client, accessToken)
+
+        describe("#retrieve jobOpenings myRegistrations") {
+            context("내가 등록한 구인구직을 조회하면") {
+                it("성공한다") {
+                    client.doGet(retrieveMyRegistrationsUrl, accessToken).expectStatus().isOk.expectBody()
+                        .consumeWith { println(it) }.jsonPath("$.result").isEqualTo("SUCCESS")
+                }
+            }
+        }
+    }
+}
