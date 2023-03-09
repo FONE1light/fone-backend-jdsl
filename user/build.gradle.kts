bc2c003a23af34e81fcbc8f8de93e84ca0f9bb87
sinkyoungdeok <tkagmd1@naver.com>
2023-03-10 00:21:35 +0900

    [#207] 유저 회원가입 유닛테스트 추가

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -1,7 +1,18 @@
-plugins { kotlin("plugin.jpa") version "1.7.0" }
+plugins {
+    kotlin("plugin.jpa") version "1.7.0"
 
-tasks.test { useJUnitPlatform() }
+    id("io.kotest") version "0.3.8"
+}
 
-dependencies { implementation(project(path = ":common", configuration = "default")) }
+dependencies {
+    implementation(project(path = ":common", configuration = "default"))
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("junit", "junit", "4.13.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation("io.mockk:mockk:1.12.0")
+}
 
 tasks.bootJar { enabled = false }
---
+++ user/src/test/java/com/fone/user/application/SignUpUserFacadeTest.kt
@@ -0,0 +1,65 @@
+package com.fone.user.application
+
+import com.fone.common.entity.CategoryType
+import com.fone.common.entity.Gender
+import com.fone.user.domain.enum.Job
+import com.fone.user.domain.enum.SocialLoginType
+import com.fone.user.domain.service.SignUpUserService
+import com.fone.user.presentation.dto.SignUpUserDto.SignUpUserRequest
+import com.fone.user.presentation.dto.SignUpUserDto.SignUpUserResponse
+import com.fone.user.presentation.dto.common.UserDto
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import java.time.LocalDate
+
+class SignUpUserFacadeTest : DescribeSpec({
+    val signUpUserService: SignUpUserService = mockk()
+    val signUpUserFacade = SignUpUserFacade(signUpUserService)
+
+    describe("#signUpUserFacade") {
+        context("given request") {
+            val signUpUserRequest =
+                SignUpUserRequest(
+                    Job.ACTOR,
+                    listOf(CategoryType.ETC),
+                    "test5",
+                    LocalDate.now(),
+                    Gender.IRRELEVANT,
+                    null,
+                    "010-1234-1234",
+                    "test5@test.com",
+                    SocialLoginType.APPLE,
+                    true,
+                    true,
+                    true,
+                    "test"
+                )
+            val signUpUserResponse =
+                SignUpUserResponse(
+                    user = UserDto(
+                        id = 1,
+                        Job.ACTOR,
+                        listOf(CategoryType.ETC),
+                        "test5",
+                        LocalDate.now(),
+                        Gender.IRRELEVANT,
+                        "",
+                        "010-1234-1234",
+                        "test5@test.com",
+                        SocialLoginType.APPLE,
+                        true,
+                        true,
+                        true,
+                        true
+                    )
+                )
+            coEvery { signUpUserService.signUpUser(signUpUserRequest) } returns signUpUserResponse
+            it("return response") {
+                val result = signUpUserFacade.signUp(signUpUserRequest)
+                result shouldBe signUpUserResponse
+            }
+        }
+    }
+})
