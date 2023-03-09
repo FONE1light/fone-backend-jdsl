a7a4c1990ac3a6e6466a8f098496963309a6ca84
sinkyoungdeok <tkagmd1@naver.com>
2023-03-09 22:31:51 +0900

    [#198] 내가 등록한 프로필 조회 테스트 코드

---
+++ server/src/test/kotlin/com/fone/profile/presentation/controller/RetrieveProfileMyRegistrationControllerTest.kt
@@ -0,0 +1,31 @@
+package com.fone.profile.presentation.controller
+
+import com.fone.common.CommonUserCallApi
+import com.fone.common.CustomDescribeSpec
+import com.fone.common.IntegrationTest
+import com.fone.common.doGet
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@IntegrationTest
+class RetrieveProfileMyRegistrationControllerTest(client: WebTestClient) : CustomDescribeSpec() {
+
+    private val retrieveMyRegistrationUrl = "/api/v1/profiles/my-registrations"
+
+    init {
+        val (accessToken, email) = CommonUserCallApi.getAccessToken(client)
+
+        describe("#retrieve profile my registration") {
+            context("내가 등록한 프로필을 조회하면") {
+                it("성공한다") {
+                    client
+                        .doGet(retrieveMyRegistrationUrl, accessToken)
+                        .expectStatus().isOk
+                        .expectBody()
+                        .consumeWith { println(it) }
+                        .jsonPath("$.result")
+                        .isEqualTo("SUCCESS")
+                }
+            }
+        }
+    }
+}
