ca77a174b107366fceda7d49ee3d5282675a3f2d
sinkyoungdeok <tkagmd1@naver.com>
2023-03-09 23:08:44 +0900

    [#198] 프로필 찜하기 테스트 코드 추가

---
+++ server/src/test/kotlin/com/fone/profile/presentation/controller/WantProfileControllerTest.kt
@@ -0,0 +1,44 @@
+package com.fone.profile.presentation.controller
+
+import com.fone.common.CommonProfileCallApi
+import com.fone.common.CommonUserCallApi
+import com.fone.common.CustomDescribeSpec
+import com.fone.common.IntegrationTest
+import com.fone.common.doPost
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@IntegrationTest
+class WantProfileControllerTest(client: WebTestClient) : CustomDescribeSpec() {
+
+    private val wantUrl = "/api/v1/profiles"
+
+    init {
+        val (accessToken, email) = CommonUserCallApi.getAccessToken(client)
+        val profileId = CommonProfileCallApi.register(client, accessToken)
+
+        describe("#want profile") {
+            context("존재하는 프로필을 스크랩 하면") {
+                it("성공한다") {
+                    client
+                        .doPost("$wantUrl/$profileId/want", null, accessToken)
+                        .expectStatus().isOk
+                        .expectBody()
+                        .consumeWith { println(it) }
+                        .jsonPath("$.result")
+                        .isEqualTo("SUCCESS")
+                }
+            }
+            context("존재하지 않는 프로필을 스크랩 하면") {
+                it("실패한다") {
+                    client
+                        .doPost("$wantUrl/1231/want", null, accessToken)
+                        .expectStatus().isOk
+                        .expectBody()
+                        .consumeWith { println(it) }
+                        .jsonPath("$.result")
+                        .isEqualTo("FAIL")
+                }
+            }
+        }
+    }
+}
