aff805934578da984ce7499fff3a84aa6919c890
sinkyoungdeok <tkagmd1@naver.com>
2023-03-09 23:45:27 +0900

    [#204] 신고하기 통합테스트 추가

---
+++ server/src/test/kotlin/com/fone/report/presentation/controller/RegisterReportControllerTest.kt
@@ -0,0 +1,41 @@
+package com.fone.report.presentation.controller
+
+import com.fone.common.CommonUserCallApi
+import com.fone.common.CustomDescribeSpec
+import com.fone.common.IntegrationTest
+import com.fone.common.doPost
+import com.fone.report.domain.enum.Type
+import com.fone.report.presentation.dto.RegisterReportDto.RegisterReportRequest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@IntegrationTest
+class RegisterReportControllerTest(client: WebTestClient) : CustomDescribeSpec() {
+
+    private val registerUrl = "/api/v1/reports"
+
+    init {
+        val (accessToken, email) = CommonUserCallApi.getAccessToken(client)
+        val registerReportRequest = RegisterReportRequest(
+            reportUserId = 1,
+            type = Type.CHATTING,
+            typeId = 1,
+            inconvenients = listOf(""),
+            details = "test details"
+        )
+
+        describe("#register report") {
+            context("유효한 정보로 신고하기를 등록하면") {
+                it("성공한다") {
+                    client
+                        .doPost(registerUrl, registerReportRequest, accessToken)
+                        .expectStatus()
+                        .isOk
+                        .expectBody()
+                        .consumeWith { println(it) }
+                        .jsonPath("$.result")
+                        .isEqualTo("SUCCESS")
+                }
+            }
+        }
+    }
+}
