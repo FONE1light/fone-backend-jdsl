5073e5413269ae9d91b738b845ef4541cecd1cdf
Bob Sin <tkagmd1@naver.com>
2023-03-12 13:29:03 +0900

    [#215] 문의등록 type 유닛 테스트 (#222)
    * [#215] 문의등록 type 유닛 테스트
    
    * [#215] fix error

--- server/src/main/kotlin/com/fone/question/domain/enum/Type.kt
+++ server/src/main/kotlin/com/fone/question/domain/enum/Type.kt
@@ -1,7 +1,11 @@
 package com.fone.question.domain.enum
 
 enum class Type {
-    USE_QUESTION,
-    VOICE_OF_THE_CUSTOMER,
-    ALLIANCE,
+    USE_QUESTION, VOICE_OF_THE_CUSTOMER, ALLIANCE,
+
+    ;
+
+    companion object {
+        operator fun invoke(type: String) = Type.valueOf(type.uppercase())
+    }
 }
