c7ae5c4457994a3be35ba808d5f717c41ceaa18d
sinkyoungdeok <tkagmd1@naver.com>
2023-03-12 13:24:17 +0900

    [#215] fix error

--- server/src/main/kotlin/com/fone/question/domain/enum/Type.kt
+++ server/src/main/kotlin/com/fone/question/domain/enum/Type.kt
@@ -1,11 +1,7 @@
 package com.fone.question.domain.enum
 
-import com.fone.report.domain.enum.Type
-
 enum class Type {
-    USE_QUESTION,
-    VOICE_OF_THE_CUSTOMER,
-    ALLIANCE,
+    USE_QUESTION, VOICE_OF_THE_CUSTOMER, ALLIANCE,
 
     ;
 
