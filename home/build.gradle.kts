66712b9b782886f36dd216cb1f7e93c009de8453
DoodlesOnMyFood <57210973+DoodlesOnMyFood@users.noreply.github.com>
2023-03-21 22:23:50 +0900

    [#243] HomeController.retrieveHome 코루틴 리팩토링 (#247)
    * test(home): 간단한 코루틴 리팩토링 테스트
    
    * chore(home): ktlint failures fixed
    
    * refactor(home): 리팩토링 반영

--- home/build.gradle.kts
+++ home/build.gradle.kts
@@ -1,3 +1,13 @@
-dependencies { implementation(project(path = ":common", configuration = "default")) }
+dependencies {
+    implementation(project(path = ":common", configuration = "default"))
+
+    // logging
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+
+    // tests
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation("io.mockk:mockk:1.12.0")
+}
 
 tasks.bootJar { enabled = false }
