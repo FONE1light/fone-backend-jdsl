5aee800fab5babbb1726aea02325bd08576c69ba
jisoo choi <fyimbtmn@gmail.com>
2023-03-23 20:36:28 +0900

    build(common): remove unnecessary dependencies

--- server/src/main/kotlin/com/fone/filmone/config/SwaggerConfig.kt
+++ server/src/main/kotlin/com/fone/filmone/config/SwaggerConfig.kt
@@ -25,7 +25,6 @@ import com.fone.user.presentation.dto.SignInUserDto.SignInUserResponse
 import com.fone.user.presentation.dto.SignUpUserDto.SignUpUserResponse
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
-import lombok.RequiredArgsConstructor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.Pageable
@@ -38,24 +37,21 @@ import springfox.documentation.schema.AlternateTypeRule
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.service.ApiKey
 import springfox.documentation.service.AuthorizationScope
-import springfox.documentation.service.Response
 import springfox.documentation.service.SecurityReference
 import springfox.documentation.service.SecurityScheme
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-import java.util.Arrays
 
 @Configuration
 @EnableSwagger2
-@RequiredArgsConstructor
 class SwaggerConfig(
     private val typeResolver: TypeResolver,
 ) {
 
     @Bean
-    fun api(): Docket? {
+    fun api(): Docket {
         val commonResponse = setCommonResponse()
         return Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
             .additionalModels(
@@ -105,18 +101,16 @@ class SwaggerConfig(
             ).consumes(getConsumeContentTypes()).produces(getProduceContentTypes())
             .apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.basePackage("com.fone"))
             .paths(PathSelectors.ant("/**")).build()
-            .securityContexts(Arrays.asList(securityContext()))
-            .securitySchemes(Arrays.asList<SecurityScheme>(apiKey()))
+            .securityContexts(listOf(securityContext()))
+            .securitySchemes(listOf<SecurityScheme>(apiKey()))
     }
 
-    private fun setCommonResponse(): List<Response> {
-        val list: MutableList<Response> = ArrayList()
-        list.add(ResponseBuilder().code("200").description("정상 처리(성공)").build())
-        list.add(ResponseBuilder().code("401").description("토큰 만료 또는 비정상 토큰 또는 권한 없음").build())
-        list.add(ResponseBuilder().code("404").description("존재하지 않는 api 요청 ").build())
-        list.add(ResponseBuilder().code("500").description("내부 서버 오류(문의 필요)").build())
-        return list
-    }
+    private fun setCommonResponse() = listOf(
+        ResponseBuilder().code("200").description("정상 처리(성공)").build(),
+        ResponseBuilder().code("401").description("토큰 만료 또는 비정상 토큰 또는 권한 없음").build(),
+        ResponseBuilder().code("404").description("존재하지 않는 api 요청 ").build(),
+        ResponseBuilder().code("500").description("내부 서버 오류(문의 필요)").build()
+    )
 
     private fun apiKey(): ApiKey {
         return ApiKey("JWT", "Authorization", "header")
@@ -127,27 +121,21 @@ class SwaggerConfig(
             .forPaths(PathSelectors.any()).build()
     }
 
-    private fun defaultAuth(): List<SecurityReference>? {
+    private fun defaultAuth(): List<SecurityReference> {
         val authorizationScope = AuthorizationScope("global", "accessEverything")
         val authorizationScopes = arrayOfNulls<AuthorizationScope>(1)
         authorizationScopes[0] = authorizationScope
-        return Arrays.asList(SecurityReference("JWT", authorizationScopes))
+        return listOf(SecurityReference("JWT", authorizationScopes))
     }
 
-    private fun getConsumeContentTypes(): Set<String> {
-        val consumes: MutableSet<String> = HashSet()
-        consumes.add("application/json;charset=UTF-8")
-        consumes.add("application/x-www-form-urlencoded")
-        return consumes
-    }
+    private fun getConsumeContentTypes(): Set<String> = setOf(
+        "application/json;charset=UTF-8",
+        "application/x-www-form-urlencoded"
+    )
 
-    private fun getProduceContentTypes(): Set<String> {
-        val produces: MutableSet<String> = HashSet()
-        produces.add("application/json;charset=UTF-8")
-        return produces
-    }
+    private fun getProduceContentTypes(): Set<String> = setOf("application/json;charset=UTF-8")
 
-    private fun apiInfo(): ApiInfo? {
+    private fun apiInfo(): ApiInfo {
         return ApiInfoBuilder().title("REST API Document")
             .description("work in progress").termsOfServiceUrl("localhost").version("1.0").build()
     }
