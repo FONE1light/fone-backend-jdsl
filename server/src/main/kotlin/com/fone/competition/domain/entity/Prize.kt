b46944fe08aa0273a12cd55ea4008947fa9b2858
DoodlesOnMyFood <57210973+DoodlesOnMyFood@users.noreply.github.com>
2023-04-07 00:28:10 +0900

    fix(server): testing User with unique constraint (#257)

--- server/src/main/kotlin/com/fone/competition/domain/entity/Prize.kt
+++ server/src/main/kotlin/com/fone/competition/domain/entity/Prize.kt
@@ -13,7 +13,7 @@ import javax.persistence.Table
 
 @Entity
 @Table(name = "competition_prizes")
-data class Prize(
+class Prize(
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,5 +31,20 @@ data class Prize(
 
     fun addCompetition(competition: Competition) {
         this.competition = competition
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Prize
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
     }
 }
