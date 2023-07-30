be80bc5c87e0527d4226986eaacca5fd5663c09b
DoodlesOnMyFood <fyimbtmn@gmail.com>
2023-07-30 15:43:36 +0900

    구조 리팩토링

--- server/src/main/kotlin/com/fone/user/application/SignInUserFacade.kt
+++ server/src/main/kotlin/com/fone/user/application/SignInUserFacade.kt
@@ -1,15 +1,28 @@
 package com.fone.user.application
 
+import com.fone.user.domain.service.OauthValidationService
 import com.fone.user.domain.service.SignInUserService
 import com.fone.user.presentation.dto.SignInUserDto.EmailSignInUserRequest
+import com.fone.user.presentation.dto.SignInUserDto.SignInUserResponse
 import com.fone.user.presentation.dto.SignInUserDto.SocialSignInUserRequest
 import org.springframework.stereotype.Service
 
 @Service
 class SignInUserFacade(
     private val signInUserService: SignInUserService,
+    private val oauthValidationService: OauthValidationService,
 ) {
 
-    suspend fun signIn(request: EmailSignInUserRequest) = signInUserService.signInUser(request)
-    suspend fun signIn(request: SocialSignInUserRequest) = signInUserService.signInUser(request)
+    suspend fun signIn(request: EmailSignInUserRequest): SignInUserResponse {
+        val user = signInUserService.getUser(request)
+        signInUserService.validate(request, user)
+        return signInUserService.generateResponse(user)
+    }
+
+    suspend fun signIn(request: SocialSignInUserRequest): SignInUserResponse {
+        val email = oauthValidationService.getEmail(request)
+        val user = signInUserService.getUser(request, email)
+        signInUserService.validate(request, user)
+        return signInUserService.generateResponse(user)
+    }
 }
--- server/src/main/kotlin/com/fone/user/domain/service/SignInUserService.kt
+++ server/src/main/kotlin/com/fone/user/domain/service/SignInUserService.kt
@@ -13,6 +13,7 @@ import com.fone.user.presentation.dto.SignInUserDto.EmailSignInUserRequest
 import com.fone.user.presentation.dto.SignInUserDto.SignInUserResponse
 import com.fone.user.presentation.dto.SignInUserDto.SocialSignInUserRequest
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.server.ServerWebInputException
 import java.util.concurrent.TimeUnit
 
@@ -24,43 +25,40 @@ class SignInUserService(
     private val oauthValidationService: OauthValidationService,
 ) {
 
-    suspend fun signInUser(request: EmailSignInUserRequest): SignInUserResponse {
+    @Transactional(readOnly = true)
+    suspend fun getUser(request: EmailSignInUserRequest): User {
         with(request) {
-            val user =
-                userRepository.findByEmailAndLoginType(email, LoginType.PASSWORD) ?: throw NotFoundUserException()
-            validate(user)
-            return generateResponse(user)
+            return userRepository.findByEmailAndLoginType(email, LoginType.PASSWORD) ?: throw NotFoundUserException()
         }
     }
 
-    suspend fun signInUser(request: SocialSignInUserRequest): SignInUserResponse {
+    @Transactional(readOnly = true)
+    suspend fun getUser(request: SocialSignInUserRequest, email: String): User {
+        return userRepository.findByEmailAndLoginType(email, request.loginType) ?: throw NotFoundUserException()
+    }
+
+    suspend fun validate(request: SocialSignInUserRequest, user: User) {
         with(request) {
-            val email = oauthValidationService.getEmail(request)
-            val user =
-                userRepository.findByEmailAndLoginType(email, loginType) ?: throw NotFoundUserException()
-            validate(user)
-            return generateResponse(user)
+            if (loginType == LoginType.PASSWORD) {
+                throw ServerWebInputException("소셜 로그인 타입이 필요합니다.")
+            }
+            if (!oauthValidationService.isValidTokenSignIn(loginType, accessToken, user.email)) {
+                throw InvalidTokenException()
+            }
         }
     }
 
-    private suspend fun SocialSignInUserRequest.validate(user: User) {
-        if (loginType == LoginType.PASSWORD) {
-            throw ServerWebInputException("소셜 로그인 타입이 필요합니다.")
-        }
-        if (!oauthValidationService.isValidTokenSignIn(loginType, accessToken, user.email)) {
-            throw InvalidTokenException()
-        }
-    }
-
-    private suspend fun EmailSignInUserRequest.validate(user: User) {
-        val isValid = user.password != null &&
-            PasswordService.isValidPassword(this.password, user.password!!)
-        if (!isValid) {
-            throw InvalidTokenException("유효하지 않은 인증시도입니다.")
+    suspend fun validate(request: EmailSignInUserRequest, user: User) {
+        with(request) {
+            val isValid = user.password != null &&
+                PasswordService.isValidPassword(this.password, user.password!!)
+            if (!isValid) {
+                throw InvalidTokenException("유효하지 않은 인증시도입니다.")
+            }
         }
     }
 
-    private fun generateResponse(user: User): SignInUserResponse {
+    fun generateResponse(user: User): SignInUserResponse {
         val token = jwtUtils.generateUserToken(user.email, user.roles.map { Role(it) }.toList())
 
         redisRepository.setValue(
