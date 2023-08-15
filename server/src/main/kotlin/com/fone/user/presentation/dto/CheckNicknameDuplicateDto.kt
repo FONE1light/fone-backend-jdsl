2a7cc1ff99fd92e651d1b0151f9a84ff5cac6ee4
Bob Sin <tkagmd1@naver.com>
2023-08-16 00:57:47 +0900

    [FO-229] 닉네임 유효성 검사 API에 글자수 체크 추가 (#290)
    * [FO-229] 닉네임 유효성 검사 API에 글자수 체크 추가
    
    * [FO-229] 닉네임 유효성 검사 API에 글자수 체크 추가
    
    * [FO-229] fix test fail

--- server/src/main/kotlin/com/fone/user/presentation/dto/CheckNicknameDuplicateDto.kt
+++ server/src/main/kotlin/com/fone/user/presentation/dto/CheckNicknameDuplicateDto.kt
@@ -1,12 +1,14 @@
 package com.fone.user.presentation.dto
 
 import io.swagger.annotations.ApiModelProperty
+import org.hibernate.validator.constraints.Length
 import javax.validation.constraints.NotEmpty
 
 class CheckNicknameDuplicateDto {
 
     data class CheckNicknameDuplicateRequest(
         @field:NotEmpty(message = "닉네임은 필수 값 입니다.")
+        @field:Length(min = 3, max = 8)
         @ApiModelProperty(
             value = "닉네임",
             example = "테스트닉네임",
