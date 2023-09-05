efcb70ef13245a478286f6046275aa892eaf7b14
DoodlesOnMyFood <fyimbtmn@gmail.com>
2023-09-06 01:07:01 +0900

    build: 불필요한 설정 제거

--- sms/build.gradle.kts
+++ sms/build.gradle.kts
@@ -1,17 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    kotlin("jvm")
-    kotlin("plugin.spring")
-}
-
-group = "com.fone"
-version = "1.0.0"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
 dependencies {
     compileOnly(project(path = ":common", configuration = "default"))
     testImplementation(project(path = ":common", configuration = "default"))
@@ -25,15 +11,4 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
     testImplementation("io.mockk:mockk:1.12.0")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
