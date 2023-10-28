7426ddc28aa7560770271f95b716c5f7d5948e7b
sinkyoungdeok <tkagmd1@naver.com>
2023-10-29 00:24:29 +0900

    [FO-672] 문자 코드 생성 버그 수정

--- server/src/main/kotlin/com/fone/user/domain/repository/MessageRepository.kt
+++ server/src/main/kotlin/com/fone/user/domain/repository/MessageRepository.kt
@@ -8,5 +8,5 @@ interface MessageRepository
 
 private val rng = Random(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC))
 fun generateRandomCode(): String {
-    return String.format("%05d", rng.nextInt(0, 1000000))
+    return String.format("%06d", rng.nextInt(0, 1000000))
 }
