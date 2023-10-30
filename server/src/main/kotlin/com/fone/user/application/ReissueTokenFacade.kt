f6da345153e82f2ca43cc488ce7f1b0bf2c8e4cb
sinkyoungdeok <tkagmd1@naver.com>
2023-10-31 00:22:04 +0900

    [FO-579] 유저의 최종접속일을 추적하기위해서 DB필드 추가 및 기능 추가

--- server/src/main/kotlin/com/fone/user/application/ReissueTokenFacade.kt
+++ server/src/main/kotlin/com/fone/user/application/ReissueTokenFacade.kt
@@ -1,14 +1,22 @@
 package com.fone.user.application
 
+import com.fone.common.jwt.Token
 import com.fone.user.domain.service.ReissueTokenService
+import com.fone.user.domain.service.SignInUserService
 import com.fone.user.presentation.dto.ReissueTokenDto
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ReissueTokenFacade(
     private val reissueTokenService: ReissueTokenService,
+    private val signInUserService: SignInUserService,
 ) {
 
-    suspend fun reissueToken(request: ReissueTokenDto.ReissueTokenRequest) =
-        reissueTokenService.reissueToken(request)
+    @Transactional
+    suspend fun reissueToken(request: ReissueTokenDto.ReissueTokenRequest): Token {
+        val token = reissueTokenService.reissueToken(request)
+        signInUserService.signInUser(token)
+        return token
+    }
 }
