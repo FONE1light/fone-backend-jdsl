0309300441ec9c5b7c97d8acc13351175260642d
sinkyoungdeok <tkagmd1@naver.com>
2023-11-04 11:02:14 +0900

    [FO-685] 구인구직 모집 2차 MVP 개선 변경사항 반영 - 기존 모델 swagger 추가

--- server/src/main/kotlin/com/fone/jobOpening/presentation/dto/RegisterJobOpeningDto.kt
+++ server/src/main/kotlin/com/fone/jobOpening/presentation/dto/RegisterJobOpeningDto.kt
@@ -10,23 +10,37 @@ import com.fone.jobOpening.domain.entity.JobOpeningScrap
 import com.fone.jobOpening.presentation.dto.common.JobOpeningDto
 import com.fone.jobOpening.presentation.dto.common.WorkDto
 import com.fone.user.domain.enum.Job
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
 
 class RegisterJobOpeningDto {
 
     data class RegisterJobOpeningRequest(
+        @Schema(description = "모집제목", example = "많은 이들의 시선보다 ..")
         val title: String,
+        @Schema(description = "작품의 성격", example = "[\"ACTOR\",\"ACTRESS\"]")
         val categories: List<CategoryType>,
-        @DateTimeFormat(pattern = "yyyy-MM-dd") val deadline: LocalDate?,
+        @Schema(description = "모집 마감일", example = "2021-10-10")
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        val deadline: LocalDate?,
+        @Schema(description = "모집배역", example = "30대 중반 경찰")
         val casting: String?,
+        @Schema(description = "모집인원", example = "1")
         val numberOfRecruits: Int,
+        @Schema(description = "성별", example = "MAN")
         val gender: Gender,
+        @Schema(description = "최대 나이", example = "40")
         val ageMax: Int,
+        @Schema(description = "최소 나이", example = "20")
         val ageMin: Int,
+        @Schema(description = "경력", example = "NEWCOMER")
         val career: Career,
+        @Schema(description = "모집유형", example = "ACTOR")
         val type: Type,
+        @Schema(description = "모집분야", example = "[\"PLANNING\",\"SCENARIO\"]")
         val domains: List<DomainType>?,
+        @Schema(description = "작품정보")
         val work: WorkDto,
     ) {
         fun toEntity(userId: Long): JobOpening {
--- server/src/main/kotlin/com/fone/jobOpening/presentation/dto/common/WorkDto.kt
+++ server/src/main/kotlin/com/fone/jobOpening/presentation/dto/common/WorkDto.kt
@@ -1,18 +1,30 @@
 package com.fone.jobOpening.presentation.dto.common
 
 import com.fone.jobOpening.domain.entity.Work
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class WorkDto(
+    @Schema(description = "제작", example = "FONE")
     val produce: String,
+    @Schema(description = "제목", example = "작품 제목")
     val workTitle: String,
+    @Schema(description = "연출", example = "이하은")
     val director: String,
+    @Schema(description = "장르", example = "스릴러")
     val genre: String,
+    @Schema(description = "로그라인", example = "자폐 스펙트럼 장애를 가진 변호사의 이야기")
     val logline: String?,
+    @Schema(description = "촬영위치", example = "서울")
     val location: String?,
+    @Schema(description = "촬영기간", example = "2021-10-10 ~ 2021-10-11")
     val period: String?,
+    @Schema(description = "급여", example = "추후협의")
     val pay: String?,
+    @Schema(description = "상세요강", example = "상세내용")
     val details: String,
+    @Schema(description = "담당자", example = "김매니저")
     val manager: String,
+    @Schema(description = "이메일", example = "test@test.com")
     val email: String,
 ) {
     constructor(
