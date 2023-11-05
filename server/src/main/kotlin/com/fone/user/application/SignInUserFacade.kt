61c40213f834af51226cb39393176b60a7feae18
sinkyoungdeok <tkagmd1@naver.com>
2023-11-05 11:03:53 +0900

    [FO-579] 유저 최종접속일 추적을 reissue 뿐만아니라 일반 로그인에도 추가한다

--- server/src/main/kotlin/com/fone/user/application/SignInUserFacade.kt
+++ server/src/main/kotlin/com/fone/user/application/SignInUserFacade.kt
@@ -6,6 +6,7 @@ import com.fone.user.presentation.dto.SignInUserDto.EmailSignInUserRequest
 import com.fone.user.presentation.dto.SignInUserDto.SignInUserResponse
 import com.fone.user.presentation.dto.SignInUserDto.SocialSignInUserRequest
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class SignInUserFacade(
@@ -13,16 +14,20 @@ class SignInUserFacade(
     private val oauthValidationService: OauthValidationService,
 ) {
 
+    @Transactional
     suspend fun signIn(request: EmailSignInUserRequest): SignInUserResponse {
         val user = signInUserService.getUser(request)
         signInUserService.validate(request, user)
+        signInUserService.signInUser(user)
         return signInUserService.generateResponse(user)
     }
 
+    @Transactional
     suspend fun signIn(request: SocialSignInUserRequest): SignInUserResponse {
         val principal = oauthValidationService.getPrincipal(request.loginType, request.accessToken)
         val user = signInUserService.getUser(principal)
         signInUserService.validate(request, user)
+        signInUserService.signInUser(user)
         return signInUserService.generateResponse(user)
     }
 }
