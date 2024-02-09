741430f94487bc064072953006d7042f99a7ef6c
Bob Sin <tkagmd1@naver.com>
2024-02-09 18:01:08 +0900

    [FO-905] (모집) 지역 시군구 api response 변경 (#406)

--- server/src/main/kotlin/com/fone/jobOpening/presentation/dto/LocationDto.kt
+++ server/src/main/kotlin/com/fone/jobOpening/presentation/dto/LocationDto.kt
@@ -4,18 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 class LocationDto {
     data class RetrieveRegionsResponse(
-        @Schema(description = "지역 (deprecated 예정)", example = "서울특별시")
-        val result: List<String>,
-
         @Schema(description = "지역", example = "서울특별시")
         val regions: List<String>,
     )
 
     data class RetrieveDistrictsResponse(
-        @Schema(description = "시군구 (deprecated 예정)", example = "강남구")
-        val result: List<String>,
-
         @Schema(description = "시군구", example = "강남구")
-        val district: List<String>,
+        val districts: List<String>,
     )
 }
