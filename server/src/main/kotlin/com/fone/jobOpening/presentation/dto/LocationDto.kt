ac9d06b176ccbc85e5ee8e2843a631c7a08c2505
sinkyoungdeok <tkagmd1@naver.com>
2024-02-25 12:43:44 +0900

    모집 테스트코드 추가

--- server/src/main/kotlin/com/fone/jobOpening/presentation/dto/LocationDto.kt
+++ server/src/main/kotlin/com/fone/jobOpening/presentation/dto/LocationDto.kt
@@ -4,12 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 class LocationDto {
     data class RetrieveRegionsResponse(
-        @Schema(description = "지역", example = "서울특별시")
+        @Schema(description = "지역", example = "[\"서울특별시\"]")
         val regions: List<String>,
     )
 
     data class RetrieveDistrictsResponse(
-        @Schema(description = "시군구", example = "강남구")
+        @Schema(description = "시군구", example = "[\"강남구\"]")
         val districts: List<String>,
     )
 }
