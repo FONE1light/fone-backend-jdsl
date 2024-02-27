1cace00925e9e45c956eae93788ebcf94fb02054
sinkyoungdeok <tkagmd1@naver.com>
2024-02-27 20:05:01 +0900

    cors enable

---
+++ common/src/main/kotlin/com/fone/common/config/CorsFilter.kt
@@ -0,0 +1,27 @@
+package com.fone.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Component
+class CorsFilter {
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val corsConfig = CorsConfiguration()
+        corsConfig.addAllowedOrigin("*")
+        corsConfig.maxAge = 8000L
+        corsConfig.addAllowedMethod(HttpMethod.GET)
+        corsConfig.addAllowedMethod(HttpMethod.POST)
+        corsConfig.addAllowedMethod(HttpMethod.OPTIONS)
+        corsConfig.addAllowedHeader("Content-Type")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+
+        return CorsWebFilter(source)
+    }
+}
